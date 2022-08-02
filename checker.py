920e0cfb7f8473a6f892e063c2ccb47e2e0b80f6
= <=>
2022-08-02 11:47:50 +0300

    From win

---
+++ checker.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import clickhouse_driver as ch
+
+class Checker:
+    def __init__(self, campaignId=None, campaignName=None):
+        self.campaignId = campaignId
+        self.campaignName =- campaignName
+        
+    def checkId(self):
+        with open('./queries/q_campaign_check.sql', 'r') as f:
+            q = f.read()
+
+        sqlCH = ch.Client(host="192.168.235.72",
+                            user="valferov",
+                            password="bUfMupl1kDgK1337")
+
+        df = pd.DataFrame(sqlCH.execute(q))
+        if df.loc[df[0]==self.campaignId].empty:
+            return False
+        else:
+            return True
